ceeb1d512d1d527bf9f0abc1959e4503394d3b8f
Jitendradhiman <jkdiith@gmail.com>
2020-07-08 19:37:51 +0530

    update listening tests V2

--- test_code_fix_ui/action.php
+++ test_code_fix_ui/action.php
@@ -83,7 +83,7 @@ if(isset($_SESSION['userdata']) && !isset($_POST['submit']))
 <!DOCTYPE html>
 <html lang="en">
 <head>
-<title>MOS test</title>
+<title>AB Test</title>
 <meta charset="utf-8">
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css">
